58441327fcf31a00dba14a53a8e63b5058544411
WonGoo <yuhwg1214@gmail.com>
2020-02-15 19:10:15 +0900

    24.4

--- cal.py
+++ cal.py
@@ -1,11 +1,22 @@
+from django.utils import timezone
 import calendar
 
 
-class Calendar:
+class Day:
+    def __init__(self, number, past):
+        self.number = number
+        self.past = past
+
+    def __str__(self):
+        return str(self.number)
+
+
+class Calendar(calendar.Calendar):
     def __init__(self, year, month):
+        super().__init__(firstweekday=6)
         self.year = year
         self.month = month
-        self.day_names = ("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")
+        self.day_names = ("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat")
         self.months = (
             "January",
             "February",
@@ -20,6 +31,22 @@ class Calendar:
             "November",
             "December",
         )
+
+    def get_days(self):
+        weeks = self.monthdays2calendar(self.year, self.month)
+        days = []
+        for week in weeks:
+            for day, _ in week:
+                now = timezone.now()
+                today = now.day
+                month = now.month
+                past = False
+                if month == self.month:
+                    if day <= today:
+                        past = True
+                new_day = Day(day, past)
+                days.append(new_day)
+        return days
 
     def get_month(self):
         return self.months[self.month - 1]

513fdb6181eb8aa4b1787f7941b2ebb8cb4270a2
WonGoo <yuhwg1214@gmail.com>
2020-02-20 23:24:47 +0900

    FINISHEDgit add .

--- config/settings.py
+++ config/settings.py
@@ -182,9 +182,11 @@ if not DEBUG:
     AWS_STORAGE_BUCKET_NAME = "airbnb-clone-wongoo"
     AWS_AUTO_CREATE_BUCKET = True
     AWS_BUCKET_ACL = "public-read"
-
+    AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
+
+    # Sentry
 
     sentry_sdk.init(
         dsn=os.environ.get("SENTRY_URL"),

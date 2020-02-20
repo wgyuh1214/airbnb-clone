2c7b5d5b4d462228bad33bf50d61dd4517ebe916
WonGoo <yuhwg1214@gmail.com>
2020-02-20 21:31:43 +0900

    26.8 edit2

--- config/settings.py
+++ config/settings.py
@@ -179,6 +179,7 @@ AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = "airbnb-clone-wongoo"
 AWS_AUTO_CREATE_BUCKET = True
+AWS_BUCKET_ACL = None
 
 if not DEBUG:
 
